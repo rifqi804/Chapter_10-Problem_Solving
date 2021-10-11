bb3ae987b39ad7f80785c6a7fd7081172e6b3a5c
Rifqi Andhika Pratama <83459256+rifqi804@users.noreply.github.com>
2021-10-11 13:52:52 +0700

    no message

---
+++ C10-Problem_Solving/Assets/Scripts/TitikController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitikController : MonoBehaviour
+{
+    private Rigidbody2D rigidBody2D;
+
+    public float xInitialForce;
+    public float yInitialForce;
+
+
+    //Mereset posisi dari objek titik
+    private void ResetTitik()
+    {
+        transform.position = Vector2.zero;
+
+        rigidBody2D.velocity = Vector2.zero;
+    }
+
+
+    //Menggerakan bola
+    void PushTitik()
+    {
+        float InitialForce = Random.Range(-yInitialForce, yInitialForce);
+
+        float randomDirection = Random.Range(0, 2);
+        if(randomDirection < 0.1f)
+        {
+            // Gunakan gaya untuk menggerakkan bola ini.
+            rigidBody2D.AddForce(new Vector2(-xInitialForce, -yInitialForce));
+        }
+        else
+        {
+            rigidBody2D.AddForce(new Vector2(xInitialForce, yInitialForce));
+        }
+    }
+
+
+    void Restart()
+    {
+        ResetTitik();
+
+        Invoke("PushTitik", 2);
+    }
+
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+
+        Restart();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
